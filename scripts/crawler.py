2544ffd1e19bfee00e8170819121f5b837cb98e7
walker088 <hanxu2121@gmail.com>
2018-06-29 12:59:19 +0800

    ageTable

--- scripts/crawler.py
+++ scripts/crawler.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 from pprint import pprint
 from bs4 import BeautifulSoup as bs
+import numpy as np
 import requests, re, csv
 
 url = "https://www.plurk.com/p/mtxvw5"
 
-def writeCsv(fileName, path='./results/'):
-    pass
+def writeCsv(header, Dict, fileName, path='../results/'):
+    with open(path+fileName, 'a', newline='') as csvFile:
+        csvWriter = csv.writer(csvFile)
+        csvWriter.writerow(header)
+        for key in Dict.keys():
+            array = np.asarray(Dict[key])
+            Age = key
+            High = np.amax(array)
+            Low = np.amin(array)
+            Medium = np.median(array)
+            UQ = np.percentile(array, 75)
+            LQ = np.percentile(array, 25)
+            Counts = len(array)
+            csvWriter.writerow([Age,High, Low, Medium, UQ, LQ, Counts])
 
 def replyFormat(reply):
     regex = re.compile("^\d*([\u2E80-\u9FFF]|)/[\u2E80-\u9FFF]*/[\u2E80-\u9FFF]*")
@@ -14,8 +27,49 @@ def replyFormat(reply):
         return True
     return False
 
+def clean(Lst):
+    return list(set(Lst))
+
+def mapDctToEmptyLst(Lst):
+    Dct = {}
+    for item in Lst:
+        Dct[item] = []
+    return Dct
+
+def convertSalary(string):
+    regex = re.compile("^\d*([K]|)")
+    match = re.search(regex, string)
+    if match != None:
+        try:
+            salary = match.group(0)
+            return int(string[:2])*1000
+        except:
+            #print(string)
+            pass
+    return False
+
 def buildAgeTable(replyLst):
-    pass
+    ageLst = []
+    srcLst = []
+    header = ['Age', 'High', 'Low', 'Medium', 'UQ', 'LQ', 'Count']
+    fileName = 'ageTable.csv'
+    for item in replyLst:
+        try:
+            rawLst = item.split("/")
+            age = int(rawLst[0][:2])
+            ageLst.append(age)
+            srcLst.append(rawLst)
+        except ValueError as verr:
+            print("ValueError", item)
+    ageLst = clean(ageLst)
+    ageDct = mapDctToEmptyLst(ageLst)
+    for item in srcLst:
+        age = int(item[0][:2])
+        salary = convertSalary(item[-1])
+        if salary != False:
+            ageDct[age].append(salary)
+    writeCsv(header, ageDct, fileName)
+    pprint(ageDct)
 
 def buildLocationTable(replyLst):
     pass
@@ -30,7 +84,6 @@ def crawler():
                 bsObj.find("section", {"id":"plurk_responses"}) \
                      .findAll("span", {"class":"plurk_content"}) \
                      if replyFormat(reply.text)==True]
-    pprint(replyLst)
     return replyLst
 
 def main():
